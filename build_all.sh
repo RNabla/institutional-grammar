c66145e450379f185aea834f67193a33b39f6975
Andrzej Nowikowski <andrzej.nowikowski@gmail.com>
2019-12-15 19:02:28 +0100

    build script

--- build_all.sh
+++ build_all.sh
@@ -1,19 +1,12 @@
 python -m venv .env
 source .env/bin/activate
 
-cd ig-annotator
-python setup.py sdist
-pip install dist/*
-rm -rf dist/
-rm -rf *egg-info/
-pip install -r requirements.txt
+cd ig-cogito
+chmod +x ./build.sh
+./build.sh
 cd ..
 
-cd MAE_to_CSV/src
-python setup.py sdist
-pip install dist/*
-rm -rf dist/
-rm -rf *egg-info/
-cd ..
-pip install -r requirements.txt
+cd ig-relations
+chmod +x ./build.sh
+./build.sh
 cd ..
