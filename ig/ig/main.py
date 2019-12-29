e3f9aad22ae1317d17677b2557d8b1eeb08304c8
Andrzej Nowikowski <andrzej.nowikowski@gmail.com>
2019-12-29 22:24:24 +0100

    schema fix

--- ig/ig/main.py
+++ ig/ig/main.py
@@ -7,7 +7,7 @@ import igrelations.main
 
 def setup_io(output_dir_path):
     if output_dir_path.exists() and not output_dir_path.is_dir():
-        raise IOError(f'{output_dir} does exist and is not a directory')
+        raise IOError(f'{output_dir_path} does exist and is not a directory')
     output_dir_path.mkdir(parents=True,exist_ok=True)
 
 def get_output_filepath(input_file_path, output_dir_path, suffix):
@@ -32,14 +32,14 @@ def create_relations(csv_file_path, output_dir):
 @click.command()
 @click.argument("input_file", type=click.Path(exists=True))
 @click.argument("output_dir", type=click.Path())
-def console_entry(input_file, output_dir, mae_schema):
+def console_entry(input_file, output_dir):
     input_file_path = Path(input_file)
     output_dir_path = Path(output_dir)
     setup_io(output_dir_path)
     mae_file_path = get_output_filepath(input_file_path, output_dir_path, '_mae.xml')
     create_mae_file(input_file_path, mae_file_path)
     xls_file_path = get_output_filepath(input_file_path, output_dir_path, '_excel.xls')
-    create_xls_file(mae_file_path, xls_file_path, mae_schema=mae_schema)
+    create_xls_file(mae_file_path, xls_file_path)
     csv_file_path = get_output_filepath(input_file_path, output_dir_path, '_csv.csv')
     create_csv_file(xls_file_path, csv_file_path)
     create_relations(csv_file_path, output_dir_path / 'relations')
